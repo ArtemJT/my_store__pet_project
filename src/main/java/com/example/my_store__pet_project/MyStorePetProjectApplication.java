700a6cf624f6e37aab8545348e846bcac0dc8795
Artem Kovalov <artemkov1990@gmail.com>
2023-03-15 12:47:04 +0200

    Initial commit

---
+++ src/main/java/com/example/my_store__pet_project/MyStorePetProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.my_store__pet_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyStorePetProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyStorePetProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/my_store__pet_project/MyStorePetProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.my_store__pet_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyStorePetProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
